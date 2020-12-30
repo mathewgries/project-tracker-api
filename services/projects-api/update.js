8e1bd21b59c95b85ff14b58c9a8f433baba713a9
mathewgries <mathewgries@gmail.com>
2020-12-30 16:59:00 -0500

    Fixing stupid issue

--- services/projects-api/update.js
+++ services/projects-api/update.js
@@ -5,9 +5,6 @@ export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be updated
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       projectId: event.pathParameters.id
